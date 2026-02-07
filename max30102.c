12b618a71e33453e0b0619f440c1a8f5023cbefe
shi hao <i.shihao.999@gmai.com>
2026-02-07 08:39:15 -0500

    max30102: add general access routines
    Added general access routines for max30102
    for driver device tree bindings as well as
    register i2c client using i2c apis

--- max30102.c
+++ max30102.c
@@ -3,14 +3,55 @@
 #include <linux/init.h>
 #include <linux/i2c.h>
 
+
+static int max30102_probe(struct i2c_client *client)
+{
+	pr_info("Driver initialized\n");
+	return 0;
+}
+
+static void max30102_remove(struct i2c_client *client)
+{
+	pr_info("Driver removed\n");
+	return;
+}
+
+static void max30102_shutdown(struct i2c_client *client)
+{
+	pr_info("Shutting down!\n");
+	return;
+}
+
+static const struct i2c_device_id max30102_idtable[] = {
+	{ "adi, max30102"},
+	{ }
+};
+
+MODULE_DEVICE_TABLE(i2c, max30102_idtable);
+
+static struct i2c_driver max30102_driver = {
+	.driver = {
+		.name = "max30102",
+		.pm = NULL
+	},
+	.id_table = max30102_idtable ,
+	.probe = max30102_probe ,
+	.remove = max30102_remove,
+	.shutdown = NULL
+};
+
 static int __init max30102_init(void)
 {
-	return 0;
+	int ret;
+	
+	ret = i2c_add_driver(&max30102_driver);
+
+	return ret;
 }
 
 static void __exit max30102_exit(void)
 {
-	return;
+	return i2c_del_driver(&max30102_driver);
 }
 
 module_init(max30102_init);
